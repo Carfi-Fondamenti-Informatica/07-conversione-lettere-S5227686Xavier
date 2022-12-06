874d8a1d28e09d88266d4d7cc9f62814f6eaba1b
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-06 18:17:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  bool b;
+    char a,ris;
+    cout<<"inserire un carattere:"<<endl;
+    cin>>a;
+    b=conv(a);
+    while (b==true) {
+        if (a >= 'a' && a <= 'z') {
+            ris = a - 32;
+            cout<<ris<<endl;
+        } else if (a >= 'A' && a <= 'Z') {
+            ris = a + 32;
+            cout<<ris<<endl;
+        }
+        return ris;
+    }
+
+    cout << "errore" << endl;
   return 0;
 }

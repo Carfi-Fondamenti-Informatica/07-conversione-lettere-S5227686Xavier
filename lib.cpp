40856daec1ff48a5f420d9ba65852185170b97f9
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-06 18:18:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
 
+#include "lib.h"
+bool conv(char c) {
+    if (c >= 'a' && c <= 'z') {
+        return true;
+    }else if (c >= 'A' && c <= 'Z') {
+        return true;
+    }else{
+        return false;
+    }
+}

37e7b81d8348346b5478be0ad854c65b5ddda3b6
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-09 01:34:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,18 @@
 
 #include "lib.h"
-unsigned char ris;
-
-bool conv(char c) {
-    if (c >= 'a' && c <= 'z') {
-        ris = c-32 ;
-        return true;
-    }else if (c >= 'A' && c <= 'Z') {
-        ris = c+32 ;
+bool check(int c){
+    if((c>=65&&c<=90)||(c>=97&&c<=122)) {
         return true;
     }else{
         return false;
     }
+}
+char conv(char c) {
+    char ris;
+    if (c >= 'A' && c <= 'Z'){
+        ris = c + 32;
+    }else if(c>='a'&& c<='z') {
+        ris = c - 32;
+    }return ris;
+
 }

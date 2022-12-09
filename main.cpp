0f3ea2280d2ed60bacc67beb93cb0004d137222c
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2022-12-09 01:34:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-extern char ris;
 
 int main() {
-    char a;
-    char b;
-    cout<<"inserire un carattere:"<<endl;
-    cin>>a;
-    b= conv(a);
-    if (b==true) {
-        cout<<ris<<endl;
-        }else{
-        cout << "errore" << endl;
+    char c;
+    char v;
+    cout<<"inserire carattere:"<<endl;
+    cin>>c;
+    if(check(c)){
+        v=conv(c);
+        cout<<v<<endl;
+    }else{
+        cout<<"errore"<<endl;
     }
+
     return 0;
 }
